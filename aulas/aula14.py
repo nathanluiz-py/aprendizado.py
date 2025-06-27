e46e38295d81d6549d646f41c0fac37be97ae8d6
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-06-26 22:52:29 -0300

    dia 3

--- aulas/aula14.py
+++ aulas/aula14.py
@@ -1,8 +1,9 @@
 a = 'A'   #posicao0
 b = 'B'   #posicao1
-c = 1.1 #posicao2
-string = 'a={} ,b={}, c={:.2f}'
-formato = string.format(a,b,c)
+c = 1.1   #posicao2
+string = 'a={n1} ,b={n1}, c={n1}, {n1},{n1},{n1}' #indices,ele busca pela posicao que a variavel esta 
+# formato = string.format(a,b,c) #aqui e o argumento,ele puxa da variavel os valores mais tamb pode ser declarado a mao 
+formato = string.format(n1=a,n2=b,n3=c) #parametro nomeados,todos parametros devem ser nomeados caso seja usado assim
 
 
 print(formato)
---
+++ aulas/aula15.py
@@ -0,0 +1,2 @@
+teste = input('Qual seu nome ? ')
+print(f'Seu nome e {teste}')
