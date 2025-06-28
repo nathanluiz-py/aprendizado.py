51af69517d4d202460a277a9c2fdf3f5f417f2fa
Nathan.dev <nathanluiz.dev@gmail.com>
2025-06-28 16:50:48 -0300

    dia 5

--- aulas/aula22.py
+++ aulas/aula22.py
@@ -1,9 +1,6 @@
 # Operadores lógicos
-# and (e) or (ou) not (não)
-# and - Todas as condições precisam ser
-# verdadeiras.
-# Se qualquer valor for considerado falso,
-# a expressão inteira será avaliada naquele valor
+# or (ou)
+# considera se caso alguma das expressoes sao verdadeiras uma (ou) outra
 # São considerados falsy (que vc já viu)
 # 0 0.0 '' False
 # Também existe o tipo None que é
