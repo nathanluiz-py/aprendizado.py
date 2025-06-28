fa63e7c4b5539bc9e922562d8992f19a99923b88
Nathan.dev <nathanluiz.dev@gmail.com>
2025-06-28 16:51:42 -0300

    dia 5

---
+++ aulas/aula23.py
@@ -0,0 +1,12 @@
+# operador logico 'not'
+# usado para inverter expressoes
+# not true = false
+# not false = true
+
+senha =  input('digite uma senha: ')
+
+if not senha:
+    print('voce nao digitou a senha')
+
+else :
+    print('bem vindo')
