2aa5b95475280b0ef4f53c4ce188a6292da8ce40
Nathan.dev <nathanluiz.dev@gmail.com>
2025-06-30 14:51:29 -0300

    comando sep e end

--- aulas/aula2.py
+++ aulas/aula2.py
@@ -1,4 +1,4 @@
-#comando sep='' serve para colocar algo entre os argumentos como ,.- por padrao caso nao coloque nada vai ser um espaço vazio
+#comando sep='' serve para colocar algo entre os argumentos como ,.- por padrao caso nao coloque nada vai ser um espaço vazio   
 #comando end='' serve para colocar algo no final dos arguemntos como ,.- e sempre bom colocar /n depois para ter a quebra de linha 
 print(12,34, sep='-',end='.\n')
 print(23,54, sep='-', end='.\n')
