c8c765e41309c4cdbce1d42f5b0d7c87cd6d2374
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-07-01 15:47:58 -0300

    formatacao com f-strings

---
+++ aulas/aula26.py
@@ -0,0 +1,25 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a 
+"""
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}') #largura fixa para preencher a esquerda
+print(f'{variavel:<10}.') #largura fixa para preencher a direita
+print(f'{variavel:^10}.')#ao centro
+print(f'{1000.4873648123746:0=+10,.1f}')#formatar numero
+print(f'O hexadecimal de 1500 é {1500:020X}') #variavel e depois coloca o numero de casas desejadas,sempre comecar com zero
+print(f'{variavel!r}')
+#caso queira preencher com algo so colocar um caractere antes dossinais ><^
