825deefd30c686ca475641ed75dabd1a4c232573
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-07-02 16:07:16 -0300

    id e None

---
+++ aulas/aula31.py
@@ -0,0 +1,20 @@
+"""Add commentMore actions
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+#ID
+# v1 = 'a'
+# print(id(v1))
+
+
+nome = input('Digite seu nome: ')
+
+if nome is None:
+    
+    print('Nome não definido.')
+
+elif nome is not None :
+    print ('ola')
