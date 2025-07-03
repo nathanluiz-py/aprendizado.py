a411ccf67e9d4a994a6735ada920ffe542f6d760
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-07-03 18:18:30 -0300

    operadores de atreibuicoes

---
+++ aulas/aula36.py
@@ -0,0 +1,39 @@
+"""
+Operadores de atreibuicoes
++= , -= , *= , /= , //= , **= , %=
+
+"""
+
+contador = 10
+contador = 2 #igual novo valor
+print(contador)
+
+
+contador = 10
+contador *= 2 #multiplicacao
+print(contador)
+
+contador = 10
+contador -= 2 #adicao
+print(contador)
+
+
+contador = 10
+contador -= 2 #subtracao
+print(contador)
+
+contador = 10
+contador /= 2 #divisao com ponto
+print(contador)
+
+contador = 10
+contador //= 2 #divisao em inteiro
+print(contador)
+
+contador = 10 
+contador **= 2  #potenciacao
+print(contador)
+
+contador = 10
+contador %= 2  #resto de divisao
+print(contador)
