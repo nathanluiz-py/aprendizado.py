7378cb081cfc2485b376a699edb5ccff8b084322
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-07-09 15:59:23 -0300

    for

---
+++ aulas/aula43.py
@@ -0,0 +1,15 @@
+texto = 'python'
+
+for letra in texto:
+    print(letra)
+
+#---------------------------------------------------------
+
+texto = 'python'
+novo_texto = ''
+
+
+for letra in texto :
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto)
