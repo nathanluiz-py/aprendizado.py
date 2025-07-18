45290bebc10aa5e9d86ccbafe528be739dd4b7d2
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-07-18 17:47:06 -0300

    tuplas

---
+++ aulas/aula52.py
@@ -0,0 +1,10 @@
+'''
+Tupla e uma lista que nao e possivel alterar os valores
+da para converter lista em tupla = nomes=tuple(nomes)
+ou
+nomes = list(nomes)
+'''
+
+nomes = ('nathan','luiz','aquino')
+
+print(nomes)
