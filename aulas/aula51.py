2c53b219dca38e9c04bf94dc822c0ce479f78595
NathanLuizz <nathan.aquino@sga.pucminas.br>
2025-07-18 17:26:23 -0300

    desacompamento e acoplamento

--- aulas/aula51.py
+++ aulas/aula51.py
@@ -1,6 +1,9 @@
 '''
 Introducao ao desempacotamento + tuples(tuplas)
 
+_ = para pular(substitua a variavel por _ ou *e uma variavel)
+_,n2,_ = ['Nathan','Luiz','Aquino']
+print(n2)
 '''
-_,n,_= ['Nathan','Luiz','Aquino']
-print(n)
+n1,n2,n3 = ['Nathan','Luiz','Aquino']
+print()
